a3fb02f4e685e02aaa2883ca8e4289221015cb07
latkina <latkina.aa@edu.spbstu.ru>
2023-04-16 20:35:46 +0300

    Лабораторная 4 (выполнена)

--- Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -10,7 +10,27 @@ def car_paths(n: int, m: int) -> List[List[int]]:
 
     :return: Новую таблицу с посчитанным количеством маршрутов в каждую клетку
     """
-    ...  # TODO решить задачу с помощью динамического программирования
+    table_result = [[0] * m for _ in range(n)]
+
+    def single_routes(table) -> List[List[int]]:
+        for j in range(1, m):
+            table[0][j] = 1
+        for i in range(1, n):
+            table[i][0] = 1
+
+    if n == 1 or m == 1:
+        single_routes(table_result)
+        return table_result
+
+    table_result[0][0] = 1  # значение задано для правильного расчета кол-ва маршрутов до [1][1]
+    single_routes(table_result)
+    for i in range(1, n):
+        for j in range(1, m):
+            table_result[i][j] = table_result[i - 1][j] + table_result[i][j - 1] + table_result[i - 1][j - 1]
+
+    table_result[0][0] = 0  # точка в которой находится автомобиль не имеет маршрутов
+
+    return table_result
 
 
 if __name__ == '__main__':
